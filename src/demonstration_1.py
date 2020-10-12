8e0abc5df3005c0175be3db44f3b131ac63cc021
ianbautista <ianfbautista@gmail.com>
2020-10-12 14:24:02 -0700

    solved demo 1

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -18,12 +18,38 @@ Given the binary tree [5,12,32,None,None,8,4],
 
 your function should return the depth = 3.
 """
+
+
 class BinaryTreeNode:
     def __init__(self, value=0, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
 
-def maxDepth(self, root):
-    # Your code here
+    def maxDepth(self, root):
+        # Your code here
+        # base case?
+        # what's the depth of an empty tree?
+        if root is None:
+            return 0
+        # how do we get to our base case?
+        # move down the tree all the way until we get to
+        # the node's at the bottom
+        # we need the height of left side
+        left_height = self.maxDepth(root.left)
+        # we need the height of the right side
+        right_height = self.maxDepth(root.right)
 
+        larger_height = max(left_height, right_height)
+
+        return 1 + larger_height
+
+
+tree = BinaryTreeNode(5)
+tree.left = BinaryTreeNode(12)
+right = BinaryTreeNode(32)
+right.right = BinaryTreeNode(4)
+right.left = BinaryTreeNode(8)
+tree.right = right
+
+print(tree.maxDepth(tree))
